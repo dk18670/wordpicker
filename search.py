a7a5b3e672c0a8aa515efd671daef4f63b90b96a
Keith Hollis <keith.hollis@gmail.com>
2019-08-07 15:36:45 +0000

    Fixed bug with multi pattern search.

--- search.py
+++ search.py
@@ -1,68 +1,42 @@
 import csv, re, datetime, collections, copy
 
-def encounter(s):
-  ret = collections.Counter()
-  for c in s:
-    ret[c] += 1
-  return ret
+class WPCounter(collections.Counter):
+  def __init__(self,s):
+    super(WPCounter,self).__init__(self)
+    for c in s:
+      self[c] += 1
 
-def decounter(counter):
-  return ''.join([k*v for k,v in counter.items()])
+  def __repr__(self):
+    return 'WPCounter('+''.join([k*v for k,v in self.items()])+')'
 
-def canmatch(chars,rack):
-  #print('chars,rack:',chars,rack)
-  rack = rack.copy()
-  # check that all chars are also in rack
-  for c in chars:
-    if c=='.' or c==' ':
-      continue
-    if not rack[c]:
-      c = '.'
-      if not rack[c]:
-        return False
-    rack[c] -= 1
-  return True
+  def __add__(self,s):
+    for c in s:
+      self[c] += 1
+    return self
+
+  def __sub__(self,s):
+    for c in s:
+      self[c] -= 1
+    return self
+
+  def __contains__(self,s):
+    cpy = collections.Counter(self)
+    for c in s:
+      if c=='.' or c==' ':
+        continue
+      if not cpy[c]:
+        c = '.'
+        if not cpy[c]:
+          return False
+      cpy[c] -= 1
+    return True
 
 def gen_match(chars,mask):
-  #print 'gen_match:', chars, mask
   # remove the matched chars and leave the . chars
   # eg mask='P.AY.R' then match='.R..E.'
-  match = ''
-  for i in xrange(len(mask)):
-    match += chars[i] if mask[i]=='.' else '.'
-  return match
+  return ''.join([(chars[i] if m=='.' else '.') for i,m in enumerate(mask)])
 
-def search(rack,patt):
-  if not rack and not patt:
-    return None
-
-  min_len = 1
-  max_len = len(rack) if rack else 15
-
-  if rack:
-    # strip out any unwanted characters
-    rack = re.sub('[^A-Z\.?_\-]','',rack.upper())
-    # replace ?_- chars with .
-    rack = re.sub('[?_\-]', '.', rack)
-  if patt:
-    beg = patt.startswith('$')
-    end = patt.endswith('$')
-    # strip out any unwanted characters
-    patt = re.sub('[^A-Z\.?_\-\[\]]','',patt.upper())
-    # replace ?_- chars with .
-    patt = re.sub('[?_\-]', '.', patt)
-    # compose a mask to remove matched chars
-    mask = re.sub('\[.*?\]','.',patt)
-    # if beginning match then add ^ prefix
-    if beg: patt = '^'+patt
-    # if end match then add $ suffix
-    if end: patt = patt+'$'
-    min_len = min(min_len,len(mask))
-    max_len += len(mask)
-    #print mask
-
-  rack = encounter(rack)
-
+def fetch_matching_words(rack, min_len, max_len, patt=None, mask=None):
   matches = []
   with open('sowpods.txt') as file:
     reader = csv.reader(file)
@@ -76,11 +50,51 @@ def search(rack,patt):
           #print(word,patt,m.group())
           match = gen_match(m.group(), mask)
           chars = word[:m.start()]+match+word[m.end():]
-          if not rack or canmatch(chars,rack):
+          if not rack or chars in rack:
             matches.append(word)
       else:
-        if not rack or canmatch(word,rack):
+        if not rack or word in rack:
           matches.append(word)
+  return matches
+
+def search(rack,patt):
+  if not rack and not patt:
+    return None
+
+  min_len = 1
+  max_len = 15
+
+  if rack:
+    # strip out any unwanted characters
+    rack = re.sub('[^A-Z\.?_\-]','',rack.upper())
+    # replace ?_- chars with .
+    rack = re.sub('[?_\-]', '.', rack)
+
+    rack = WPCounter(rack)
+
+    min_len = 1
+    max_len = len(rack)
+
+  if patt:
+    beg = patt.startswith('$')
+    end = patt.endswith('$')
+    # strip out any unwanted characters
+    patt = re.sub('[^A-Z\.?_\-\[\]]','',patt.upper())
+    # replace ?_- chars with .
+    patt = re.sub('[?_\-]', '.', patt)
+    # compose a mask to remove matched chars
+    mask = re.sub('\[.*?\]','.',patt)
+    # if beginning match then add ^ prefix
+    if beg: patt = '^'+patt
+    # if end match then add $ suffix
+    if end: patt = patt+'$'
+    min_len = len(mask)
+    max_len += len(mask)
+    #print(mask)
+  else:
+    mask = None
+
+  matches = fetch_matching_words(rack, min_len, max_len, patt, mask)
 
   return matches if len(matches) else None
 
@@ -89,55 +103,36 @@ def search2(rack,patts):
   if not rack:
     return None
 
-  # strip out any unwanted characters
-  rack = re.sub('[^A-Z\.?_\-]','',rack.upper())
-  # replace ?_- chars with .
-  rack = re.sub('[?_\-]', '.', rack)
+  min_len = 1
+  max_len = 15
 
-  rack = encounter(rack)
+  if rack:
+    # strip out any unwanted characters
+    rack = re.sub('[^A-Z\.?_\-]','',rack.upper())
+    # replace ?_- chars with .
+    rack = re.sub('[?_\-]', '.', rack)
 
-  def scan(rack, min_len, max_len, patt=None, mask=None):
-    #print 'scan:', rack, min_len, max_len, patt, mask
-    matches = []
-    with open('sowpods.txt') as file:
-      reader = csv.reader(file)
-      for row in reader:
-        word = row[0]
-        if not min_len <= len(word) <= max_len:
-          continue
-        if patt:
-          m = re.search(patt, word)
-          if m:
-            # remove the matched chars and leave the . chars, eg patt='P.AY.R' match='.R..E.'
-            match = ''
-            for i in xrange(len(mask)):
-              match += m.group()[i] if mask[i]=='.' else '.'
-            chars = word[:m.start()]+match+word[m.end():]
-            if canmatch(chars,rack):
-              matches.append(word)
-        else:
-          if canmatch(word,rack):
-            matches.append(word)
- 
-    return matches
+    max_len = len(rack)
+
+    rack = WPCounter(rack)
 
   def expand(prefix,mask,results,masks,rack):
-    #print 'expand:', prefix, mask, masks, rack
+    #print('expand:', prefix, mask, masks, rack)
     mask = mask+masks[0]
     if len(results)==1:
       for word in results[0]:
         match = gen_match(prefix+word, mask)
-        if canmatch(match, rack):
+        if match in rack:
           yield prefix + word
     else:
       for word in results[0]:
         match = gen_match(prefix+word, mask)
-        if canmatch(match, rack):
+        if match in rack:
           for ex in expand(prefix+word+' ', mask, results[1:], masks[1:], rack):
             yield ex
 
-  if not patts or len(patts)==0:
-    return scan(rack, 1, len(rack))
+  if not patts:
+    return fetch_matching_words(rack, min_len, max_len)
 
   now = datetime.datetime.now()
   results =[]
@@ -155,23 +150,21 @@ def search2(rack,patts):
     if beg: patt = '^'+patt
     # if end match then add $ suffix
     if end: patt = patt+'$'
-    min_len = len(mask)
-    max_len = len(rack)+len(mask)
-    matches = scan(rack, min_len, max_len, patt, mask)
+    matches = fetch_matching_words(rack, len(mask), max_len+len(mask), patt, mask)
     results.append(matches)
     masks.append(mask)
-  #print 'compiling results took:', datetime.datetime.now()-now
+  #print('compiling results took:', datetime.datetime.now()-now)
   now = datetime.datetime.now()
   n = 1
   for result in results:
-    #print len(result), result
+    #print(len(result), result)
     n *= len(result)
-  #print n, 'combinations..'
+  #print(n, 'combinations..')
   matches = []
   for group in expand('','',results,masks,rack):
     matches.append(group)
   td = datetime.datetime.now()-now
   microseconds = (td.days*24*60*60 + td.seconds)*1000000 + td.microseconds
-  #print td, '= %d per second' % (n*1000000/microseconds)
+  #print(td, '= %d per second' % (n*1000000/microseconds))
   now = datetime.datetime.now()
   return matches
--- tests/test_search.py
+++ tests/test_search.py
@@ -8,7 +8,6 @@ class TestCases(unittest.TestCase):
     result = gen_match('PAYER', 'P.Y.R')
     self.assertEqual(result, '.A.E.')
 
-  #@unittest.skip
   def test_canmatch(self):
     result = 'PAYER' in WPCounter('P.Y.R')
     self.assertEqual(result, True)
@@ -28,10 +27,10 @@ class TestCases(unittest.TestCase):
     results = ['AD', 'AGED', 'DA', 'DAE', 'DAG', 'DAM', 'DAME', 'DE', 'DEG', 'ED', 'EGAD', 'GAD', 'GADE', 'GAED', 'GAMED', 'GED', 'MAD', 'MADE', 'MADGE', 'MEAD', 'MED']
     self.assertEqual(matches, results)
 
-  @unittest.skip
+  #@unittest.skip
   def test_rack_with_multi_pattern(self):
-    matches = search2('INTEALI', ['$..$','$.....$'])
-    results = ['EH', 'EIK', 'ET', 'ETH', 'HE', 'HET', 'HEY', 'HI', 'HIE', 'HIKE', 'HIT', 'HYE', 'HYKE', 'HYTE', 'IT', 'KET', 'KEY', 'KHET', 'KHI', 'KI', 'KIT', 'KITE', 'KITH', 'KITHE', 'KY', 'KYE', 'KYTE', 'KYTHE', 'TE', 'THE', 'THEY', 'THY', 'TI', 'TIE', 'TIKE', 'TYE', 'TYKE', 'YE', 'YEH', 'YET', 'YETI', 'YIKE', 'YITE']
+    matches = search2('CHELSAY', ['$...$','$....$'])
+    results = [ 'ACH LEYS', 'ACH LYES', 'ACH LYSE', 'ACH SLEY', 'ALS YECH', 'AYS LECH', 'CEL ASHY', 'CEL HAYS', 'CEL SHAY', 'CEL YAHS', 'CHA LEYS', 'CHA LYES', 'CHA LYSE', 'CHA SLEY', 'CHE LAYS', 'CHE SLAY', 'CLY HAES', 'CLY SHEA', 'EAS LYCH', 'ECH LAYS', 'ECH SLAY', 'EHS ACYL', 'EHS CLAY', 'EHS LACY', 'ELS ACHY', 'ELS CHAY', 'HAY CELS', 'HES ACYL', 'HES CLAY', 'HES LACY', 'HEY LACS', 'HYE LACS', 'LAC HEYS', 'LAC HYES', 'LAH SCYE', 'LAH SYCE', 'LAS YECH', 'LAY SECH', 'LES ACHY', 'LES CHAY', 'LEY CASH', 'LEY CHAS', 'LYE CASH', 'LYE CHAS', 'SAC HYLE', 'SAE LYCH', 'SAL YECH', 'SAY LECH', 'SEA LYCH', 'SEC HYLA', 'SEL ACHY', 'SEL CHAY', 'SEY CHAL', 'SHE ACYL', 'SHE CLAY', 'SHE LACY', 'SHY ALEC', 'SHY LACE', 'SLY ACHE', 'SLY EACH', 'SYE CHAL', 'YAH CELS', 'YEH LACS', 'YES CHAL']
     self.assertEqual(matches, results)
 
   def test_pattern(self):
